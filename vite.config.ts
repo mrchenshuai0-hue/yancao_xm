0050a9d0ec64f03d42c45ba38e851d70f0ee0c57
mrchenshuai0-hue <mr.chenshuai0@gmail.com>
2026-04-01 07:43:41 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: '/yancao_xm/'    define: {
+    base: '/yancao_xm/',    define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
